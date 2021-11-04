7e905e32be59155698657e748ad7a85e81bac47c
SonOfMagic <qq1324318532@gmail.com>
2021-11-04 16:49:06 +0800

    feat: add a custom generator

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
-  }
+  },
+  setupFilesAfterEnv: ['./jest.setup.js']
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,29 @@
 import { Command } from 'commander'
+import { writeCloudbaserc } from './templete/cloudbaserc'
 const { version } = require('../package.json')
-
 const program = new Command()
 program.version(version)
-
+const cwd = process.cwd()
 program
-  .option('-gen, --generate', 'generate cloudbaserc.json')
+  .command('generate')
+  .alias('gen')
+  .description('generate cloudbaserc.json')
+  .option('-dir [path]', 'the output functions dir')
+  .action(async (args) => {
+    let distDir
+    if (args.Dir) {
+      distDir = args.Dir
+    }
+    await writeCloudbaserc(cwd, distDir)
+    console.log('Successfully generate cloudbaserc.json')
+  })
 
 program.parse(process.argv)
 
-const options = program.opts()
+// const options = program.opts()
 
-console.log(options)
+// console.log(options)
+
+// if (options.generate) {
+//   writeCloudbaserc(cwd)
+// }

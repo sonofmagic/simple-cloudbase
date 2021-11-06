0d2b912469e2c22578e9bff694c1384158a4ea2e
SonOfMagic <qq1324318532@gmail.com>
2021-11-07 01:44:12 +0800

    feat: add more cli command

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import execa from 'execa'
-import { STCB_EXECUTABLE_PATH, esmProjectPath } from './util'
+import { STCB_EXECUTABLE_PATH, esmProjectPath, fixturesPath } from './util'
 
 test('default cli generate cloudbaserc.json ', async () => {
   process.chdir(esmProjectPath)
@@ -25,5 +25,22 @@ test('default build all with params', async () => {
     '-outdir',
     'dist'
   ]) // .stdout?.pipe(process.stdout)
+  expect(true).toBe(true)
+})
+
+test('default init project', async () => {
+  process.chdir(fixturesPath)
+  try {
+    await execa(STCB_EXECUTABLE_PATH, ['init', 'ts-starter']) // .stdout?.pipe(process.stdout)
+  } catch (error) {
+    console.error(error)
+  }
+
+  expect(true).toBe(true)
+})
+
+test('default dev mode', async () => {
+  process.chdir(fixturesPath)
+  await execa(STCB_EXECUTABLE_PATH, ['dev']) // .stdout?.pipe(process.stdout)
   expect(true).toBe(true)
 })
--- test/util.ts
+++ test/util.ts
@@ -5,3 +5,5 @@ export const nativeProjectPath = path.resolve(__dirname, 'fixtures/native')
 export const esmProjectPath = path.resolve(__dirname, 'fixtures/esm')
 
 export const STCB_EXECUTABLE_PATH = path.resolve(__dirname, '../bin/stcb')
+
+export const fixturesPath = path.resolve(__dirname, 'fixtures')

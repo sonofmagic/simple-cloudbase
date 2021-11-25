475d459b6f682e62d8bff9cd60bc1ca2f1b88b46
ice breaker <1324318532@qq.com>
2021-11-25 11:39:07 +0800

    feat: commit mock proxy link

--- src/mock/openapi.ts
+++ src/mock/openapi.ts
@@ -1,38 +1,58 @@
 import type * as Cloud from 'wx-server-sdk'
 
-function createProxy (openapi: Record<string, any> = {}) {
-  const invokePath:string[] = []
-  function getProxy<T extends Record<string, any>> (target: T, prop: string | symbol | number, callTimes:number = 0): T| (()=> Promise<Record<string, any>>) {
-    invokePath.push(prop as string)
-    if (callTimes === 0) {
-      const proxy = new Proxy(target, {
-        get (t, prop) {
-          return getProxy(t, prop, 1)
-        }
-      })
-      return proxy
-    } else {
-      const path = invokePath.join('.')
-      return (...args) => {
-        return Promise.resolve({
-          errCode: 0,
-          errMsg: 'Mock Api',
-          invokePath: path,
-          args
-        })
-      }
+type AnyObject = Record<string, any>
+
+type UnionProp = string | symbol | number
+
+function getResolveData<T> (target:T, invokePath?:UnionProp[], ...args:any[]) {
+  const path = invokePath?.join('.')
+  return {
+    errCode: 0,
+    errMsg: 'Mock Api',
+    invokePath: path,
+    target,
+    args
+  }
+}
+
+function createFuncByTarget<T> (target:T, invokePath?:UnionProp[]) {
+  return function (...args:any[]) {
+    return Promise.resolve(getResolveData(target, invokePath, ...args))
+  }
+}
+
+function createProxy<T extends AnyObject> (openapi: AnyObject = {}) {
+  function createGetHandle () {
+    return function get (target:T, prop:UnionProp, receiver:any, invokePath?:UnionProp[]) {
+      invokePath?.push(prop)
+      const tmp = target ? target[prop as string] ?? {} : target
+      const wrapFunc = createFuncByTarget<T>(tmp, invokePath)
+      return getProxy(wrapFunc as unknown as T, prop, invokePath)
     }
   }
+
+  const get = createGetHandle()
+
+  function getProxy (parent: T, prop: UnionProp, invokePath?:UnionProp[]): T {
+    const proxy = new Proxy(parent, {
+      get (target:T, prop:UnionProp, receiver:any) {
+        return get(target, prop, receiver, invokePath)
+      }
+    })
+    return proxy
+  }
+
   return new Proxy(openapi, {
-    get (target, prop) {
-      return getProxy(target, prop)
+    get (target:T, prop:UnionProp, receiver:any) {
+      const invokePath:UnionProp[] = []
+      return get(target, prop, receiver, invokePath)
     }
   })
 }
 
 function getOpenapiProxy (openapi: Record<string, any> = {}) {
-  const proxy = createProxy(openapi)
-  return proxy
+  // highjack
+  return createProxy(openapi)
 }
 
 export function mock (cloud: typeof Cloud) {
--- test/mock/openapi.test.ts
+++ test/mock/openapi.test.ts
@@ -10,9 +10,18 @@ describe('[Mock](openapi) test group', () => {
     const params = {
       img: []
     }
-    const res = await cloud.openapi.search.imageSearch(params)
+
+    const res = await cloud.openapi.aaa(params)
     expect(res.errCode).toBe(0)
-    expect(res.invokePath).toBe('search.imageSearch')
+    expect(res.invokePath).toBe('aaa')
     expect(res.args[0]).toEqual(params)
+    const res1 = await cloud.openapi.search.imageSearch(params)
+    expect(res1.errCode).toBe(0)
+    expect(res1.invokePath).toBe('search.imageSearch')
+    expect(res1.args[0]).toEqual(params)
+    const res2 = await cloud.openapi.search.aaa.imageSearch(params)
+    expect(res2.errCode).toBe(0)
+    expect(res2.invokePath).toBe('search.aaa.imageSearch')
+    expect(res2.args[0]).toEqual(params)
   })
 })

d2f018669c9a89ad86bdf1fc3fc5d75e5de6fe6c
ice breaker <1324318532@qq.com>
2021-11-25 11:46:43 +0800

    feat: add create ctx function

--- src/mock/openapi.ts
+++ src/mock/openapi.ts
@@ -4,8 +4,19 @@ type AnyObject = Record<string, any>
 
 type UnionProp = string | symbol | number
 
-function getResolveData<T> (target:T, invokePath?:UnionProp[], ...args:any[]) {
-  const path = invokePath?.join('.')
+export interface IBaseContext{
+  invokePath: UnionProp[]
+  [Key:string]: any
+}
+
+function createContext ():IBaseContext {
+  return {
+    invokePath: []
+  }
+}
+
+function getResolveData<T> (target:T, ctx:IBaseContext, ...args:any[]) {
+  const path = ctx.invokePath.join('.')
   return {
     errCode: 0,
     errMsg: 'Mock Api',
@@ -15,28 +26,28 @@ function getResolveData<T> (target:T, invokePath?:UnionProp[], ...args:any[]) {
   }
 }
 
-function createFuncByTarget<T> (target:T, invokePath?:UnionProp[]) {
+function createFuncByTarget<T> (target:T, ctx:IBaseContext) {
   return function (...args:any[]) {
-    return Promise.resolve(getResolveData(target, invokePath, ...args))
+    return Promise.resolve(getResolveData(target, ctx, ...args))
   }
 }
 
 function createProxy<T extends AnyObject> (openapi: AnyObject = {}) {
   function createGetHandle () {
-    return function get (target:T, prop:UnionProp, receiver:any, invokePath?:UnionProp[]) {
-      invokePath?.push(prop)
+    return function get (target:T, prop:UnionProp, receiver:any, ctx:IBaseContext) {
+      ctx.invokePath.push(prop)
       const tmp = target ? target[prop as string] ?? {} : target
-      const wrapFunc = createFuncByTarget<T>(tmp, invokePath)
-      return getProxy(wrapFunc as unknown as T, prop, invokePath)
+      const wrapFunc = createFuncByTarget<T>(tmp, ctx)
+      return getProxy(wrapFunc as unknown as T, prop, ctx)
     }
   }
 
   const get = createGetHandle()
 
-  function getProxy (parent: T, prop: UnionProp, invokePath?:UnionProp[]): T {
+  function getProxy (parent: T, prop: UnionProp, ctx:IBaseContext): T {
     const proxy = new Proxy(parent, {
       get (target:T, prop:UnionProp, receiver:any) {
-        return get(target, prop, receiver, invokePath)
+        return get(target, prop, receiver, ctx)
       }
     })
     return proxy
@@ -44,8 +55,8 @@ function createProxy<T extends AnyObject> (openapi: AnyObject = {}) {
 
   return new Proxy(openapi, {
     get (target:T, prop:UnionProp, receiver:any) {
-      const invokePath:UnionProp[] = []
-      return get(target, prop, receiver, invokePath)
+      const ctx = createContext()
+      return get(target, prop, receiver, ctx)
     }
   })
 }
